fe7663c4fffb97bee43929fd44a53e8cb7d69387
Agustin3100 <5718@holbertonstudents.com>
2023-05-21 16:09:15 -0300

    Fix setup sh

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,5 +1,8 @@
 sudo apt-get update 
 url=$(curl -s "https://api.github.com/repositories/11180687/releases/latest" | grep -o 'https://.*hugo_extended.*_Linux-64bit.tar.gz')
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s;
+sudo mv ./bin/golangci-lint /usr/bin/;
+sudo rm -rf ./bin
 curl -s $url -L -o hugo.tar.gz
 tar -zxf hugo.tar.gz -C /usr/local/bin
 rm /usr/local/bin/README.md
